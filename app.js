933fe29665ac6f9a93fda64fcb5f5acaf435d69f
yetginakcan3 <yetginakcan3@gmail.com>
2024-07-11 12:06:14 +0300

    2. commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("middleware 1 calıstırıldı...")
-    next()
+app.use('/',(req,res,next) => {
+   console.log('loglama yapıldı...')
+   next()
 })
 
-app.use((req,res,next) => {
-    console.log("middleware 2 calıstırıldı...")
-    res.send("<h1>hello from express.js<h1>")
+app.use('/add-student',(req,res,next) => {
+    res.send('<h1>Öğrenci Eklendi<h1>')
+})
+
+app.use('/student-list',(req,res,next) => {
+    res.send("<h1>öğrenciler listesi<h1>")
+})
+
+app.use('/',(req,res,next) => {
+    res.send('<h1>Ana sayfaya hoş geldiniz.<h1>')
 })
 
 app.listen(3500, () => {
